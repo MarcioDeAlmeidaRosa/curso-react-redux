35a31d2cfa18b941b96435327a50a5f73c5ecdeb
Marcio de Almeida Rosa <marcio.almeida.rosa@gmail.com>
2025-09-24 22:00:49 -0300

    17. Primeiro Componente #02

--- fundamentos-react/src/components/basics/Primeiro.js
+++ fundamentos-react/src/components/basics/Primeiro.js
@@ -1,5 +1,13 @@
-function Primeiro(){
-    return 'Primeiro componente';
+import React from "react";
+
+function Primeiro() {
+  const message = "Seja bem vindo(a)!";
+  return (
+    <div>
+      <h2>Primeiro componente</h2>
+      <p> {message} </p>
+    </div>
+  );
 }
 
 export default Primeiro;
