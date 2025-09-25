9568be783a0a7ef249229b02b3ec9fb096059518
Marcio de Almeida Rosa <marcio.almeida.rosa@gmail.com>
2025-09-24 21:44:38 -0300

    16. Primeiro Componente #01

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -1,6 +1,13 @@
-import './index.css';
+import "./index.css";
 import React from "react";
 import { render } from "react-dom";
 
+import Primeiro from "./components/basics/Primeiro";
+
 const tag = <strong>Olá React!!!</strong>;
-render(<div>{tag}</div>, document.getElementById("root"));
+render(
+  <div>
+    <Primeiro />
+  </div>,
+  document.getElementById("root")
+);
