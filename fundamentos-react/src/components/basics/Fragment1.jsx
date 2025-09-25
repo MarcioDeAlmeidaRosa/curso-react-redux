0e0b7d18fe6ff92c9b850a177492611cf6ec1b7a
Marcio de Almeida Rosa <marcio.almeida.rosa@gmail.com>
2025-09-24 22:44:38 -0300

    20. React Fragment

---
+++ fundamentos-react/src/components/basics/Fragment1.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Fragment1(){
+    return (
+        <>
+            <h2>Fragmento forma 2</h2>
+            <p>Cuidado com este erro</p>
+        </>
+    );
+}
--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -4,6 +4,8 @@ import { render } from "react-dom";
 
 import Primeiro from "./components/basics/Primeiro";
 import ComParametro from "./components/basics/ComParametro";
+import Fragment from "./components/basics/Fragment";
+import Fragment1 from "./components/basics/Fragment1";
 
 const tag = <strong>Olá React!!!</strong>;
 render(
@@ -13,6 +15,8 @@ render(
       titulo="Meu título vai aqui"
       subtitulo="Aqui vai meu subtitulo"
     />
+    <Fragment />
+    <Fragment1 />
   </div>,
   document.getElementById("root")
 );
